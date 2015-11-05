00de4261859d680470d59e9aac7eb6a749ee0bc9
Peter Kurpiewski <pkurpiewski@gmail.com>
2015-11-05 11:31:00 -0500

    Added supplementals for week 2.

--- Week 1/Functions/functions_over_strings.ml
+++ Week 1/Functions/functions_over_strings.ml
@@ -1,5 +1,17 @@
 (*
 last_character that returns the last character of a string, assuming that the string argument is not empty;
+*)
+
+let last_character str =
+  let length = String.length str in
+  String.get str (length - 1);;
+
+(*
 string_of_bool that converts a boolean value to its string representation.
 *)
 
+let string_of_bool bool =
+  if bool then
+    "true"
+  else
+    "false";;
