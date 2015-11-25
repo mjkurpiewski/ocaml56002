990570cc9a2b1b11e126daa7fc37256c5c327a6c
Peter Kurpiewski <pkurpiewski@gmail.com>
2015-11-25 04:35:15 -0500

    Mostly done with fifo

---
+++ Week 3/Exercises/01 - Recursive Types/fifo.ml
@@ -0,0 +1,28 @@
+type queue = int list * int list;;
+
+let is_empty (front, back) =
+  match (front, back) with
+  | ([], []) -> true
+  | (_, _) -> false;;
+
+let enqueue x (front, back) =
+  (front, x :: back);;
+
+let rec listbuilder l (front, back) counter =
+  if List.length l = counter then
+    (front, List.rev back)
+  else if counter < List.length l / 2 then
+    listbuilder l (front, List.nth l counter :: back) (counter + 1)
+  else
+    listbuilder l (List.nth l counter :: front, back) (counter + 1);;
+
+let split l =
+  listbuilder l ([], []) 0;;
+
+let dequeue (front, back) =
+
+(* let dequeue q = *)
+(*   match q with *)
+(*   | ([], back) -> (List.hd (List.rev back), split back) *)
+(*   | (front, []) -> (List.hd front, split (List.rev (List.tl front))) *)
+(*   | (front, back) -> (List.hd front, split (back @ List.rev (List.tl front)));; *)
