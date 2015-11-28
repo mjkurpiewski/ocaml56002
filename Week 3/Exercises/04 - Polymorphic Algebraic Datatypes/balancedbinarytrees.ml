0a3afee94e2a5a9eae626bcc5e3677c9d9daeaf9
Peter Kurpiewski <pkurpiewski@gmail.com>
2015-11-28 17:03:53 -0500

    finished balancedbinarytrees.ml

--- Week 3/Exercises/04 - Polymorphic Algebraic Datatypes/balancedbinarytrees.ml
+++ Week 3/Exercises/04 - Polymorphic Algebraic Datatypes/balancedbinarytrees.ml
@@ -2,20 +2,19 @@ type 'a bt =
   | Empty
   | Node of 'a bt * 'a * 'a bt ;;
 
-let height (tree : 'a bt) : int =
-  let rec height_helper (tree : 'a bt) (counter : int) : int =
-    match tree with
-    | Empty -> counter
-    | Node (Empty, _, r) -> (height_helper r (counter + 1))
-    | Node (l, _, Empty) -> (height_helper l (counter + 1))
-    | Node (l, _, r) -> (height_helper l (counter + 1)) +
-                        (height_helper r (counter + 1))
-  in
-  height_helper tree 0;;
-
 let rec height t =
   match t with
   | Empty -> 0
   | Node (Empty, _, r) -> 1 + (height r)
   | Node (l, _, Empty) -> 1 + (height l)
-  | Node (l, _, r) -> 1 + (height r) + (height l);;
+  | Node (l, _, r) -> 1 + max (height r) (height l);;
+
+let rec balanced t =
+  match t with
+  | Empty -> true
+  | Node (Empty, _, Empty) -> true
+  | Node (l, _, r) ->
+    if (height l) = (height r) then
+      balanced l && balanced r
+    else
+      false;;
