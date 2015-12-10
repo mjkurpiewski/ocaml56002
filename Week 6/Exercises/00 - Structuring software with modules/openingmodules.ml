9cdfd74ef59e99bddf34688d53be5e7b1f39adfb
Peter Kurpiewski <pkurpiewski@gmail.com>
2015-12-10 13:54:14 -0500

    Finished 00/01/, working Functors -> end wk. 6

---
+++ Week 6/Exercises/00 - Structuring software with modules/openingmodules.ml
@@ -0,0 +1,11 @@
+open String;;
+open Digest;;
+open List;;
+
+let print_hashes (hashes : Digest.t list) : unit =
+  let print_hash h =
+    h
+    |> to_hex
+    |> uppercase
+    |> print_endline in
+  iter print_hash hashes;;
